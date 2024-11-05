a60e193d31e12f3ec6092a8971bbfc349d674a03
Thitipol-benz <Thitipolbenz@gmail.com>
2024-11-05 15:47:14 +0700

    ท

--- Work8/Work8.py
+++ Work8/Work8.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 blurred_image = cv2.imread('heart.jpg', cv2.IMREAD_GRAYSCALE)
 
 # Create Gaussian kernel
-kernel_size = 27
-sigma = 3.15
+kernel_size = 25
+sigma = 2.5
 gaussian_kernel = cv2.getGaussianKernel(kernel_size, sigma)
 kernel = gaussian_kernel * gaussian_kernel.T
 
@@ -18,7 +18,7 @@ H_uv = np.fft.fft2(kernel, s=blurred_image.shape)
 G_uv = np.fft.fft2(blurred_image)
 
 # Use Wiener filter
-K = 0.005
+K = 0.001
 F_uv = (G_uv / (H_uv + 1e-10)) * (np.abs(H_uv)**2 / (np.abs(H_uv)**2 + K))
 F_uv = np.fft.ifftshift(F_uv)
 restored_image = np.abs(np.fft.ifft2(F_uv))
